3b0fdd4429e171a124764b1bbac739eea92a2574
Jackietruong2805 <truonghao2805@gmail.com>
2024-03-03 10:00:28 +0700

    add Drag and Drop feature to todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-
-interface Task {
-  id: string;
-  content: string;
-  status: "todo" | "complete";
-}
+import {
+  DragDropContext,
+  DraggableLocation,
+  DropResult,
+  Droppable,
+} from "react-beautiful-dnd";
+import { Task as TaskType } from "./types/Task.type";
+import Task from "./Components/Task";
 
 function App() {
   const [edit, setEdit] = useState<boolean>(false);
   const [value, setValue] = useState<string>("");
   let todos = JSON.parse(localStorage.getItem("todos") as string) || [];
-  const [tasks, setTask] = useState<Task[]>(todos);
+  const [tasks, setTask] = useState<TaskType[]>(todos);
   const [taskId, setTaskId] = useState<string>("");
   const onEdit = (id: string, content: string) => {
     setValue(content);
@@ -20,7 +22,7 @@ function App() {
   };
   const editTask = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const isTodoExist = (todos as Task[]).find(
+    const isTodoExist = (todos as TaskType[]).find(
       (item) => value === item.content
     );
     if (isTodoExist) {
@@ -28,7 +30,7 @@ function App() {
       setEdit(false);
       return;
     }
-    todos = (todos as Task[]).map((todo) => {
+    todos = (todos as TaskType[]).map((todo) => {
       if (todo.id === taskId) {
         return {
           id: taskId,
@@ -50,7 +52,7 @@ function App() {
       content: value,
       status: "todo",
     };
-    const isTodoExist = (todos as Task[]).find(
+    const isTodoExist = (todos as TaskType[]).find(
       (item) => todo.content === item.content
     );
     if (isTodoExist) {
@@ -63,12 +65,12 @@ function App() {
     setValue("");
   };
   const deleteTask = (id: string) => {
-    todos = (todos as Task[]).filter((todo) => todo.id != id);
+    todos = (todos as TaskType[]).filter((todo) => todo.id != id);
     setTask(todos);
     localStorage.setItem("todos", JSON.stringify(todos));
   };
   const completeTask = (id: string) => {
-    todos = (todos as Task[]).map((todo) => {
+    todos = (todos as TaskType[]).map((todo) => {
       if (todo.id === id) {
         return {
           ...todo,
@@ -81,7 +83,7 @@ function App() {
     localStorage.setItem("todos", JSON.stringify(todos));
   };
   const unCompletTask = (id: string) => {
-    todos = (todos as Task[]).map((todo) => {
+    todos = (todos as TaskType[]).map((todo) => {
       if (todo.id === id) {
         return {
           ...todo,
@@ -90,6 +92,18 @@ function App() {
       }
       return todo;
     });
+    setTask(todos);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  const handleOnDragEnd = (result: DropResult) => {
+    const todos = Array.from(tasks);
+    const [reorderedItem] = todos.splice(result.source.index, 1);
+    todos.splice(
+      (result.destination as DraggableLocation).index,
+      0,
+      reorderedItem
+    );
+
     setTask(todos);
     localStorage.setItem("todos", JSON.stringify(todos));
   };
@@ -142,70 +156,35 @@ function App() {
           )}
           <div className="pt-3">
             <h3 className="mb-3 text-xl text-left text-gray-400">Todo</h3>
-            <div className="flex flex-col max-h-[30vh] overflow-auto todo">
-              {tasks.length > 0 &&
-                tasks.map((task) => {
-                  if (task.status === "todo") {
-                    return (
-                      <div
-                        key={task.id}
-                        className="px-3 py-2 mb-4 font-semibold text-white rounded-sm cursor-pointer bg-violet-500"
-                      >
-                        <div className="flex items-center justify-between">
-                          <span>{task.content}</span>
-                          <span className="flex gap-2">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke="currentColor"
-                              className="w-5 h-5"
-                              onClick={() => completeTask(task.id)}
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                              />
-                            </svg>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke="currentColor"
-                              className="w-5 h-5"
-                              onClick={() => onEdit(task.id, task.content)}
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                              />
-                            </svg>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke="currentColor"
-                              className="w-5 h-5"
-                              onClick={() => deleteTask(task.id)}
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                              />
-                            </svg>
-                          </span>
-                        </div>
-                      </div>
-                    );
-                  }
-                })}
-            </div>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+              <Droppable droppableId="droppable">
+                {(provided) => (
+                  <div
+                    className="flex flex-col max-h-[30vh] overflow-auto"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {tasks.length > 0 &&
+                      tasks.map((task, index) => {
+                        if (task.status === "todo") {
+                          return (
+                            <Task
+                              key={task.id}
+                              id={task.id}
+                              index={index}
+                              completeTask={completeTask}
+                              deleteTask={deleteTask}
+                              onEdit={onEdit}
+                              task={task}
+                            />
+                          );
+                        }
+                      })}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
           </div>
           <div className="pt-3">
             <h3 className="mb-3 text-xl text-left text-gray-400">Done</h3>
